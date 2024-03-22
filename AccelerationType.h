42dfa1765844fdaa8cf01c0a9c7bd209c42c69b7
Austin Drury <drurya96@gmail.com>
2024-03-21 22:42:30 -0500

    Fixes to Acceleration units and Grams literal
    This commit fixes the MetersPerHourSquared
    and KilometersPerHourSquared values in Acceleration.
    
    This also updates the literal for Grams from 'm' to 'g'

--- AccelerationType.h
+++ AccelerationType.h
@@ -39,11 +39,11 @@ namespace Units
 
 //https://www.nist.gov/pml/special-publication-811/nist-guide-si-appendix-b-conversion-factors/nist-guide-si-appendix-b8
 UNIT_TEMPLATE(Acceleration, StandardGravity, 9.80665, g0); // acceleration of free fall, standard (gn)
-UNIT_TEMPLATE(Acceleration, MetersPerHourSquared, 12960000.0, meter_per_hour_2); // (1 m) / (1/(3600^2) hr/sec^2)
+UNIT_TEMPLATE(Acceleration, MetersPerHourSquared, (1.0 / 12960000.0), meter_per_hour_2); // (1 m) / (1/(3600^2) hr/sec^2)
 UNIT_TEMPLATE(Acceleration, FeetPerMinuteSquared, (0.3048 / 3600.0), fpm2); // (0.3048 m/ft) / (60^2 min^2/sec)
 UNIT_TEMPLATE(Acceleration, FeetPerSecondSquared, 0.3048, fps2); // (0.3048 m/ft)
 UNIT_TEMPLATE(Acceleration, MilesPerHourSquared, (1609.344 / 12960000.0), mph2); // (1609.344 m/mi) / (3600^2 sec^2/hr)
-UNIT_TEMPLATE(Acceleration, KilometersPerHourSquared, (10.0 / 1296.0), kph2); // (1000 m/km) / (3600^2 sec^2/hr)
+UNIT_TEMPLATE(Acceleration, KilometersPerHourSquared, (1.0 / 12960.0), kph2); // (1000 m/km) / (3600^2 sec^2/hr)
 UNIT_TEMPLATE(Acceleration, InchesPerSecondSquared, 0.0254, inps2);
 UNIT_TEMPLATE(Acceleration, Galileo, 0.01, Galileo);
 UNIT_TEMPLATE(Acceleration, MetersPerSecondSquared, 1.0, mps2);
